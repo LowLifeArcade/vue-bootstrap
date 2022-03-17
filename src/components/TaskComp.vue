a688d08eb7b96cfb1014eed029423df0f04dac40
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2022-03-17 16:09:08 -0700

    update

--- src/components/TaskComp.vue
+++ src/components/TaskComp.vue
@@ -1,13 +1,31 @@
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+  message: String});
+</script>
+/**
+<div>
+// export default {
+//   name: 'TaskComp',
+//   props: {
+//     task: Object,
+//   },
+//   data() {
+//     return {
+//       message: 'hi',
+//     };
+//   },
+//   methods: {
+//     day() {
+//       this.message = this.message == 'hi' ? 'gotcha' : 'hi';
+//     },
+//   },
+// };
+// </div>
+
 <template>
   <h3>{{ task.day }}</h3>
+  <p role="button" @click="day">{{ props.message }}</p>
 </template>
-
-<script>
-export default {
-  props: {
-    task: Object,
-  },
-};
-</script>
 
 <style></style>
